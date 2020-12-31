d0a4c1570fcd2c0447d45e9afbe095e27fcaf2e8
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2020-12-30 21:52:34 -0500

    change reducer for auth

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -43,6 +43,7 @@ export const authReducer = (state = initialState, action) => {
         token: null,
         isAuthenticated: false,
         loading: false,
+        user: null,
       };
     default:
       return state;
