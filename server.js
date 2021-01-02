c938317a9791d4aa5fd0767072596c34e056e232
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2021-01-02 17:42:05 -0500

    test

--- server.js
+++ server.js
@@ -16,11 +16,13 @@ app.use("/users", require("./routes/users"));
 app.use("/stories", require("./routes/stories"));
 app.use("/contact", require("./routes/contact"));
 
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  // Set static folder
+  app.use(express.static("client/build"));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
